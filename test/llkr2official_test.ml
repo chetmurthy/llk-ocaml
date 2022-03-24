3bb6f85af08879abec7c9b956e70e194cdd77fd3
Chet Murthy <chetsky@gmail.com>
2022-03-24 14:15:31 -0700

    saving work

--- test/llkr2official_test.ml
+++ test/llkr2official_test.ml
@@ -13,6 +13,7 @@ Pcaml.inter_phrases.val := Some ";;\n" ;
 
 
 module Official = struct
+include Papr_util.Official ;
 module Implem = struct
 value pr l =
   Pr_official.(with_buffer_formatter pp_implem (l, Ploc.dummy))
@@ -28,8 +29,12 @@ value both_pr = (Implem.pr, Interf.pr) ;
 end ;
 
 value both_pa1 =
-  let (implem_pa1, interf_pa1) = PAPR.both_pa1 in
-  (fun name -> implem_pa1 ~{input_file=name}, fun name -> interf_pa1 ~{input_file=name})
+  let open Revised_rtest.Revised in
+  let pa pf input_file strm = let (ast, _) = with_input_file input_file pf strm in ast in
+  let pa1 pf input_file s = let ast = pf input_file (Stream.of_string s) in ast in
+  let implem_pa1 = pa1 (pa (Grammar.Entry.parse implem)) in
+  let interf_pa1 = pa1 (pa (Grammar.Entry.parse interf)) in
+  (implem_pa1, interf_pa1)
 ;
 
 value tests = "matrix" >::: (Papr_test_matrix.r2official both_pa1 Official.both_pr (Some Official.both_pa) ()) ;
