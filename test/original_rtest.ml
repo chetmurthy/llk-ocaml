4a98fbac60afeef938f6e450732cd521cc530307
Chet Murthy <chetsky@gmail.com>
2022-03-31 15:15:15 -0700

    meh

--- test/original_rtest.ml
+++ test/original_rtest.ml
@@ -1665,7 +1665,7 @@ MLast.SgMtyAlias loc <:vala< i >> <:vala< li >> attrs
       | "let"; rf = V (FLAG "rec"); lb = V (LIST1 let_binding SEP "and");
         "in"; ce = SELF ->
           <:class_expr< let $_flag:rf$ $_list:lb$ in $ce$ >>
-      | "let"; "open"; ovf = V (FLAG "!") "!"; alg_attrs = alg_attributes_no_anti; i = extended_longident; "in"; ce = SELF →
+      | "let"; PRIORITY 1 ; "open"; ovf = V (FLAG "!") "!"; alg_attrs = alg_attributes_no_anti; i = extended_longident; "in"; ce = SELF →
           class_expr_wrap_attrs <:class_expr< let open $_!:ovf$ $longid:i$ in $ce$ >> alg_attrs
       | x = NEXT -> x
       ]
