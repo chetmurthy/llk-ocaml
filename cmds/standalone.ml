19abef70c499a9aefac4b00df59c8d5dddca344f
Chet Murthy <chetsky@gmail.com>
2022-04-10 14:33:24 -0700

    meh

--- cmds/standalone.ml
+++ cmds/standalone.ml
@@ -1,59 +1,62 @@
-open MLast ;
-open Pcaml ;
-open Pretty;
-open Prtools;
-open Print_gram ;
-open Comp_llk ;
-open Pa_ppx_runtime_fat ;
+open MLast
+open Pcaml
+open Pretty
+open Prtools
+open Print_gram
+open Comp_llk
+open Pa_ppx_runtime_fat
 
-value _main ~{verbose} ~{input_file} ~{output_file} = 
-  let txt = match input_file with [
+module Process = struct
+
+let process verbose input_file output_file = 
+  let txt = match input_file with
         "<stdin>" -> really_input_string stdin (in_channel_length stdin)
       | _ -> RT.read_file input_file
-      ] in
-  let code = txt |> Top.codegen Ploc.dummy ~{bootstrap=True} in
-  let oc = match output_file with [
+  in
+  let code = txt |> Top.codegen Ploc.dummy ~bootstrap:true in
+  let oc = match output_file with
         "<stdout>" -> stdout
       | _ -> open_out output_file
-      ] in do {
-    output_string oc (Eprinter.apply pr_str_item Pprintf.empty_pc code) ;
-    output_string oc "\n" ;
-    flush oc
-  }
-;
+  in
+  output_string oc (Eprinter.apply pr_str_item Pprintf.empty_pc code) ;
+  output_string oc "\n" ;
+  flush oc
 
-value usage_msg = "usage" ;
-value verbose = ref False ;
-value input_file = ref "<stdin>" ;
-value output_file = ref "<stdout>" ;
-value anon_fun filename =
-  input_file.val := filename ;
+open Cmdliner
 
-value speclist =
-  [("-verbose", Arg.Set verbose, "Output debug information");
-   ("-o", Arg.Set_string output_file, "Set output file name")]
-;
 
-value main () =
-  try
-    _main ~{verbose=verbose.val} ~{input_file=input_file.val} ~{output_file=output_file.val}
-  with [
-      e ->
-      let open Printexc in
-      let loc = match e with [
-            Ploc.Exc loc _ -> loc
-          | _ -> Ploc.dummy
-          ] in
-      Fmt.(pf stderr "%sException raised:%a@;%s"
-             (Ploc.string_of_location loc)
-             Exceptions.pp e
-             (get_backtrace()))
-    ]
-;
+let verbose =
+  let doc = "Print file names as they are copied." in
+  Arg.(value & flag & info ["v"; "verbose"] ~doc)
 
-if not Sys.interactive.val then do {
-    Arg.parse speclist anon_fun usage_msg;
-    Printexc.record_backtrace True ;
-    main ()
-} else ()
+let src =
+  let doc = "Source file to process." in
+  Arg.(value & pos 0 string "<stdin>" & info [] ~docv:"SOURCE" ~doc)
+
+let dst =
+  let doc = "Destination file." in
+  Arg.(value & pos 1 string "<stdout>" & info [] ~docv:"DESTINATION" ~doc)
+
+let cmd =
+  let doc = "process command" in
+  let man = [
+    `S Manpage.s_description;
+    `P "Process a file"]
+  in
+  let info = Cmd.info "process" ~doc ~man in
+  Cmd.v info Term.(const process $ verbose $ src $ dst)
+end
+
+open Cmdliner
+
+let main_cmd =
+  let doc = "LLK cmdline" in
+  let info = Cmd.info "standalone" ~version:"%%VERSION%%" ~doc in
+  let default = Term.(ret (const (`Help (`Pager, None)))) in
+  Cmd.group info ~default [Process.cmd]
+
+if not !Sys.interactive then begin
+    Printexc.record_backtrace true ;
+    exit (Cmd.eval main_cmd)
+end
 ;
