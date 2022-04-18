6669204cd2f923d76c43b3ff431800b222cb1c6e
Chet Murthy <chetsky@gmail.com>
2022-04-17 17:39:05 -0700

    meh

--- test/hoisted_sempred_rtest.ml
+++ test/hoisted_sempred_rtest.ml
@@ -8,7 +8,7 @@ value enableEnum = ref False ;
 {foo|
 GRAMMAR Hoisted:
 EXPORT:
- stat dynamically
+ stat dynamically_stat dynamically_id dynamically_kwd
  ;
 
 stat: [ [
@@ -16,31 +16,44 @@ stat: [ [
         | enumAsKeyword -> 2
         ] ]
         ;
-identifier: [ [ LIDENT | enumAsID ] ] ;
+identifier: [ [ s = LIDENT -> s | s = enumAsID -> s ] ] ;
 
-enumAsKeyword: [ [ {enableEnum.val}? ; "enum" ] ] ;
+enumAsKeyword: [ [ {enableEnum.val}? ; s = "enum" -> s ] ] ;
 
-enumAsID: [ [ {not enableEnum.val}? ; "enum" ] ] ;
+enumAsID: [ [ {not enableEnum.val}? ; s = "enum" -> s ] ] ;
 
-dynamically: [ [ set_reference ; s = stat -> s ] ] ;
 set_reference: [ [ "set" -> enableEnum.val := True | "unset" -> enableEnum.val := False ] ] ;
+
+dynamically_stat: [ [ set_reference ; s = stat -> s ] ] ;
+dynamically_id: [ [ set_reference ; s = identifier -> s ] ] ;
+dynamically_kwd: [ [ set_reference ; s = enumAsKeyword -> s ] ] ;
 
 END ;
 |foo} ;
 ] ;
 
-value matches ~{pattern} text =
+value matches ?{quote=True} ~{pattern} text =
+  let pattern = if quote then Str.quote pattern else pattern in
   match Str.search_forward (Str.regexp pattern) text 0 with [
     _ -> True
   | exception Not_found -> False
-  ]
+]
 ;
 
-value assert_raises_exn_pattern pattern f =
+value assert_raises_exn_pattern ?{quote=True} pattern f =
   Testutil.assert_raises_exn_pred
     (fun e ->
       let s = Printexc.to_string e in
-      matches ~{pattern=pattern} s)
+      matches ~{quote=quote} ~{pattern=pattern} s)
+    f
+;
+
+value assert_raises_exn ?{quote=True} exn f =
+  Testutil.assert_raises_exn_pred
+    (fun exn' ->
+      let s = Printexc.to_string exn in
+      let s' = Printexc.to_string exn' in
+      s = s')
     f
 ;
 
@@ -61,12 +74,15 @@ value tests = "simple" >::: [
                  assert_equal 2 (pa Hoisted.stat "enum")
               })
     ; "3" >:: (fun _ -> do {
-                 enableEnum.val := True ;
-                 assert_equal 2 (pa Hoisted.dynamically "set enum")
+                 assert_equal 2 (pa Hoisted.dynamically_stat "set enum")
               })
     ; "4" >:: (fun _ -> do {
-                 enableEnum.val := True ;
-                 assert_equal 1 (pa Hoisted.dynamically "unset enum")
+                 assert_equal 1 (pa Hoisted.dynamically_stat "unset enum")
+              })
+    ; "5" >:: (fun _ -> do {
+                 assert_raises_exn_pattern
+                   "[s = identifier] expected"
+                   (fun () -> pa Hoisted.dynamically_id "set enum")
               })
     ]
 ]
